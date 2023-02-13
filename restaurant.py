4976fd276ee9a190f0b0f345520771d802a94456
Olena-Olenka <123622110+Olena-Olenka@users.noreply.github.com>
2023-02-13 17:39:30 +0200

    Add files via upload

---
+++ restaurant.py
@@ -0,0 +1,9 @@
+from random import randint
+answer = input("Welcome to Monty's.\n What would you like to eat or to drink? \n Print your answer, using commas")
+choice = answer.split(',') 
+print("Thank you for visiting Monty's!")
+for i in choice:
+  
+   print (i + '.....UAH{}'.format(randint (50,100)))
+ 
+print('Total.....UAH{}'.format(randint (50,100)))
