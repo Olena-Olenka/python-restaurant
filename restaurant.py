75ab4c72c1cb1e5190014ac32da358d3d89d3b4a
Olena-Olenka <123622110+Olena-Olenka@users.noreply.github.com>
2023-02-14 12:44:31 +0200

    Update restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -1,9 +1,9 @@
 from random import randint
 answer = input("Welcome to Monty's.\n What would you like to eat or to drink? \n Print your answer, using commas")
 choice = answer.split(',') 
-print("Thank you for visiting Monty's!")
-for i in choice:
-  
-   print (i + '.....UAH{}'.format(randint (50,100)))
- 
-print('Total.....UAH{}'.format(randint (100,500)))
+sum = 0
+for i in choice: 
+    price = randint (50,100) 
+    print (i + '.....UAH{}'.format(price))
+    sum += price
+print('Total.....UAH{}'.format(sum))
